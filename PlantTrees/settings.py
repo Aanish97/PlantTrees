792b5e5d3f6f5bab8ddd49c79f26f835a1432a52
Aanish <aanishamir@gmail.com>
2021-12-23 20:56:05 +0500

    home page added

--- PlantTrees/settings.py
+++ PlantTrees/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k69j0av-&f8akat(#wa-ljf=7=7v)2-nclb$kd=uykh4$aps-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,8 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+AUTH_USER_MODEL = "users.User"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
+from app.views import HomeView
 from users.views import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    # path('', HomeView.as_view(), name='home'),
+    path('', HomeView.as_view(), name='home'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
